793ee75e3dfb1ea831fea27b969ee6bddfcb8e0c
Daniel Monroy <fernandodms04@gmail.com>
2024-08-09 10:14:24 -0600

    Image, Hidden content and Validations

--- MiPrimeraApp/MiPrimeraApp/ContentView.swift
+++ MiPrimeraApp/MiPrimeraApp/ContentView.swift
@@ -24,6 +24,9 @@ struct ContentView: View {
     
     @State private var nombre = ""
     @State private var mensajeFinal = ""
+    @State private var showAlert = false
+    @State private var showImage = false
+    @State private var showMessage = false
     
     var body: some View {
         // Stack grabs all its available space considering other spacers and paddings. This is vertical
@@ -35,51 +38,76 @@ struct ContentView: View {
             VStack (alignment: .leading) {
                 
                 // This is the first spacer used to center vertically.
-                Spacer()
-                
-                HStack {
-                    // So now this stack is not
-                    VStack (alignment: .leading) {
-                        Text("Welcome 👋")
-                            .font(.title)
-                            .fontWeight(.bold)
-                            .padding(.vertical, 5)
-                        
-                        Text("Please enter your name")
-                            .font(.title3)
-                        
-                        TextField("Enter here", text: $nombre)
-                            .frame(height: 40)
-                            .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 6))
-                            .background(
-                                RoundedRectangle (cornerRadius: 10)
-                                    .stroke(.black, lineWidth: 2)
-                                    
-                            )
-                            .padding(.vertical)
-                        
-                        Button("Greet!") {
-                            mensajeFinal = "Nice you have your here, " + nombre
+
+
+                VStack (alignment: .leading) {
+                    
+                    Spacer()
+                    
+                    Text("Welcome 👋")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(.vertical, 5)
+                    
+                    Text("Please enter your name")
+                        .font(.title3)
+                    
+                    TextField("Enter here", text: $nombre)
+                        .frame(height: 40)
+                        .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 6))
+                        .background(
+                            RoundedRectangle (cornerRadius: 10)
+                                .stroke(.black, lineWidth: 2)
+                                
+                        )
+                        .padding(.vertical)
+                    
+                    Button("Greet!") {
+                        // Make a validation and show an alert
+                        if nombre == "" {
+                            showImage = false
+                            showMessage = false
+                            showAlert.toggle()
+                        // Name was inserted
+                        } else {
+                            mensajeFinal = "Your new home, " + nombre
+                            showMessage = true
+                            showImage = true
                         }
+                    }
                         .buttonStyle(.borderedProminent)
                         .tint(Color(hex: 0xe9dbb1))
                         .foregroundColor(.black)
                         .padding(.vertical)
-                        
+                    
+                    Spacer()
+                    
+                }
+                
+                // This is the second spacer used to center vertically.
+                VStack(alignment: .leading) {
+
+                    if showMessage {
                         Text(mensajeFinal)
                             .font(.title3)
                     }
                     
-                    // To put to the left and to extend to extend as much as possible horizontally
-                     Spacer()
+                    if showImage {
+                        Image("Gravity Falls Shack")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                    }
+                    
+                    Spacer()
                 }
-                
-                // This is the second spacer used to center vertically.
-                Spacer()
             }
         }
         .padding()
         .background(Color(hex: 0xfbf8ef))
+        // To show an alert if we don't introduce a name
+        .alert("No name specified :(", isPresented: $showAlert) {
+            Button("OK") {}
+        }
     }
 }
 
