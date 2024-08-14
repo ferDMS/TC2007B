a2773a9985d9ff2ea873fbda9bd7c582f93deeff
Daniel Monroy <fernandodms04@gmail.com>
2024-08-14 10:48:19 -0600

    Start CalcApp

---
+++ activities/CheckCalcApp/CheckCalcApp/CheckCalcAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckCalcAppApp.swift
+//  CheckCalcApp
+//
+//  Created by PEZ on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CheckCalcAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
