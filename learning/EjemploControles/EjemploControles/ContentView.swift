fd15ba738b242e5efb9e067c27acb9e0b57ef230
Daniel Monroy <fernandodms04@gmail.com>
2025-05-29 21:55:51 -0600

    Enhancements
    Add saving colors

--- learning/EjemploControles/EjemploControles/ContentView.swift
+++ learning/EjemploControles/EjemploControles/ContentView.swift
@@ -34,78 +34,126 @@ struct ContentView: View {
     // Toggle
     @State private var toggleValue : Bool = false
     
+    // List of colors
+    @State private var colorList : [Color] = []
+    
     func updateColorString() {
+        let newColorValue = 0.5 + sliderValue/(255*2)
+        
         if pickerValue == 1 {
             colorStr = "Red"
-            sliderColor = Color(red: sliderValue/255, green: 0.1, blue: 0.1)
+            sliderColor = Color(red: newColorValue, green: 0.1, blue: 0.1)
         } else if pickerValue == 2 {
             colorStr = "Green"
-            sliderColor = Color(red: 0.1, green: sliderValue/255, blue: 0.1)
+            sliderColor = Color(red: 0.1, green: newColorValue, blue: 0.1)
         } else {
             colorStr = "Blue"
-            sliderColor = Color(red: 0.1, green: 0.1, blue: sliderValue/255)
+            sliderColor = Color(red: 0.1, green: 0.1, blue: newColorValue)
         }
     }
     
     var body: some View {
-        
         // VStack for background color
         VStack {
             VStack(alignment: .leading) {
-                Spacer()
+
                 Text("Color Palette Generator 🎨")
                     .font(.largeTitle)
                     .fontWeight(.bold)
                     .foregroundColor(Color(hex: 0x1E201E))
                 
-                Text("Select a primary color")
-                    .font(.body)
-                    .padding(.top, 20)
+                ColorConfigView
                 
-                // Para seleccionar un item de una lista
-                Picker(selection: $pickerValue, label: Text("Selector")) {
-                    Text("Red").tag(1)
-                    Text("Green").tag(2)
-                    Text("Blue").tag(3)
-                }
-                    .pickerStyle(.segmented)
-                //  .pickerStyle(.wheel)
-                    .onChange(of: pickerValue) {
-                        pickerValue = pickerValue
-                        updateColorString()
-                    }
-                
-                Text("Change intensity")
-                    .padding(.top, 20)
-            
-                Slider(
-                    value: $sliderValue,
-                    in: 0...255,
-                    onEditingChanged: { editing in
-                        isEditingSlider = editing
-                    }
-                )
-                .tint(sliderColor)
-                .onChange(of: sliderValue) {
-                    if isEditingSlider {
-                        updateColorString()
-                    }
-                }
-                
-                Toggle("Inverse colors", isOn: $toggleValue) 
-                    .tint(Color(hex: 0x1E201E))
-                    .onChange(of: toggleValue) {
-                    oldValue, newValue in
-                }
-                    .padding(.top, 20)
+                ColorSavedView
                        
                 Spacer()
-                
-                
             }
         }
         .padding()
         .background(Color(hex: 0xECDFCC))
+    }
+    
+    var ColorConfigView: some View {
+        VStack(alignment: .leading) {
+            Text("Color config")
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding(.top, 20)
+            
+            Divider()
+            
+            Text("Select a primary color")
+                .font(.body)
+                .padding(.top, 20)
+            
+            // Para seleccionar un item de una lista
+            Picker(selection: $pickerValue, label: Text("Selector")) {
+                Text("Red").tag(1)
+                Text("Green").tag(2)
+                Text("Blue").tag(3)
+            }
+                .pickerStyle(.segmented)
+            //  .pickerStyle(.wheel)
+                .onChange(of: pickerValue) {
+                    pickerValue = pickerValue
+                    updateColorString()
+                }
+            
+            Text("Change intensity")
+                .padding(.top, 20)
+        
+            Slider(
+                value: $sliderValue,
+                in: 0...255,
+                onEditingChanged: { editing in
+                    isEditingSlider = editing
+                }
+            )
+            .tint(sliderColor)
+            .onChange(of: sliderValue) {
+                if isEditingSlider {
+                    updateColorString()
+                }
+            }
+            
+            HStack {
+                Button("Save color") {
+                    colorList.append(sliderColor)
+                }
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding(.vertical, 20)
+                .frame(maxWidth: .infinity)
+                .background(sliderColor)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+                .padding(.top, 20)
+            }
+        }
+    }
+    
+    var ColorSavedView: some View {
+        VStack(alignment: .leading) {
+            Text("Saved colors")
+                .padding(.top, 20)
+                .font(.title2)
+                .fontWeight(.bold)
+            
+            Divider()
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 20) {
+                    ForEach(Array(colorList.enumerated()).reversed(), id: \.offset) { index, color in
+                        Text("Color \(index + 1)")
+                            .foregroundStyle(.white)
+                            .font(.largeTitle)
+                            .frame(width: 200, height: 200)
+                            .background(color)
+                    }
+                }
+            }
+            .padding(.top, 20)
+        }
     }
 }
 
--- learning/PruebasUnitariasTest/PruebasUnitariasTest/ContentView.swift
+++ learning/PruebasUnitariasTest/PruebasUnitariasTest/ContentView.swift
@@ -13,22 +13,40 @@ struct ContentView: View {
     @State public var tip: Double
     @State public var total = 0.0
     
-    
-    func calc() -> Double {
-        if (tip < 0) {
-            return 0.0
-        } else {
-            let total = base + base * (tip / 100)
-            return total
+    func calc(base: Double, tip: Double) -> Double {
+        let amounts = inputTextFields(base: base, tip: tip)
+        let tip = amounts[1]
+        let base = amounts[0]
+        
+        if tip < 0 || tip > 100 {
+            return tip.isNaN ? 0 : 0
         }
+        return base + base * (tip / 100)
     }
+    
+    func inputTextFields(base: Double, tip: Double) -> [Double] {
+        // Define base and tip
+        return [Double(basePrice) ?? 0.0, Double(tipPercentage) ?? 0.0]
+    }
+    
+    // Input for base price
+    @State public var basePrice: String = ""
+    
+    // Input for tip percentage
+    @State public var tipPercentage: String = ""
     
     var body: some View {
         VStack {
             Text("Unit tests")
             
+            // Input
+            VStack {
+                TextField("Base price", text: $basePrice)
+                TextField("Tip percentage", text: $tipPercentage)
+            }
+            
             Button("Calc") {
-                total = calc()
+                total = calc(base: base, tip: tip)
             }
             .buttonStyle(.borderedProminent)
             
